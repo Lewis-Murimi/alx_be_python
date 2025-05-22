d09221d1ee9172ca3146885dc1c99a68f9e25176
Lewis-Murimi <lewismurimi195@gmail.com>
2025-05-22 21:29:35 +0300

    Performed basic arithmetic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,12 @@
+number1 = 10
+number2 = 5
+# Addition
+sum_result = number1 + number2
+# Subtraction
+difference_result = number1 - number2
+# Multiplication
+product_result = number1 * number2
+# Print the results
+print("Addition of", number1, "and", number2, "is", sum_result)
+print("Subtraction of", number1, "and", number2, "is", difference_result)
+print("Multiplication of", number1, "and", number2, "is", product_result)
